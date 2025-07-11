12a72bdc83578de6246e293a2c706c656aef23ec
Aliza Khan <alizaadi2@gmail.com>
2025-07-12 00:12:00 +0500

    removed navbar from /products route

--- src/Routes/routes.jsx
+++ src/Routes/routes.jsx
@@ -1,26 +1,28 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom'
 import Home from '../../pages/Home'
 import ProductsList from '../../pages/ProductsList'
 import ProductDetail from '../../pages/ProductDetail'
 import Cart from '../components/Product/cart'
 import AppNavbar from '../components/Navbar/navbar'
 import Footer from '../components/Footer/footer'
-const AppRoutes = () => {
 
+const AppRoutes = () => {
+  const location = useLocation();
   return (
     <>
-    <BrowserRouter>
-    <AppNavbar/>
-    <Routes>
-        <Route element={<Home/>} path='/'></Route>
-        <Route element={<ProductsList/>} path='/products'/>
-        <Route element={<ProductDetail/>} path='/product/:title'/>
-        <Route element={<Cart/>} path='/cart'/>
-    </Routes>
-    <Footer/>
-    </BrowserRouter>
+      {/* <BrowserRouter> */}
+        {location.pathname !== "/products" && <AppNavbar />}
+        {/* <AppNavbar/> */}
+        <Routes>
+          <Route element={<Home />} path='/'></Route>
+          <Route element={<ProductsList />} path='/products' />
+          <Route element={<ProductDetail />} path='/product/:title' />
+          <Route element={<Cart />} path='/cart' />
+        </Routes>
+        <Footer />
+      {/* </BrowserRouter> */}
     </>
   )
 }
 
-export default AppRoutes
+export default AppRoutes;
